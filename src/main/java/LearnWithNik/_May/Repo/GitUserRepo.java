e52f899d1497a3145a7710c23f5f2948cc7a66b4
NikhilPal79 <nikhilchpr7@gmail.com>
2026-03-25 12:30:24 -0400

    rest template with cashing

--- src/main/java/LearnWithNik/_May/Repo/GitUserRepo.java
+++ src/main/java/LearnWithNik/_May/Repo/GitUserRepo.java
@@ -4,6 +4,11 @@ import LearnWithNik._May.Entity.GitHubUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GitUserRepo extends JpaRepository<GitHubUser,Integer> {
+
+
+    Optional<GitHubUser> findByLogin(String login);
 }
--- src/main/java/LearnWithNik/_May/Service/GitHubService.java
+++ src/main/java/LearnWithNik/_May/Service/GitHubService.java
@@ -3,11 +3,14 @@ package LearnWithNik._May.Service;
 import LearnWithNik._May.Entity.GitHubUser;
 import LearnWithNik._May.Repo.GitUserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class GitHubService {
@@ -19,7 +22,7 @@ public class GitHubService {
     RestTemplate restTemplate;
 
     ///  Rest Template
-    public GitHubUser fetchAndPersist (String login){
+    /*public GitHubUser fetchAndPersist (String login){
 
         ResponseEntity<GitHubUser> gitHubUserResponseEntity = restTemplate.getForEntity("https://api.github.com/users/" + login, GitHubUser.class);
         if (Objects.nonNull(gitHubUserResponseEntity.getBody().getId())
@@ -28,5 +31,69 @@ public class GitHubService {
             return gitHubUserResponseEntity.getBody();
         }
         return  new GitHubUser();
+    }*/
+
+    /*
+    * Summary
+RestTemplate acts as a simple HTTP client in your service, allowing you to:
+
+Call external APIs (GitHub)
+
+Map JSON responses to your Java objects automatically
+
+Handle responses as ResponseEntity for full HTTP metadata*/
+
+
+    public GitHubUser fetchAndPersist(String login){
+
+        /// check in database first
+        Optional<GitHubUser> byLogin = githubUserRepo.findByLogin(login);
+
+        if (byLogin.isPresent()) {
+            return byLogin.get();
+        }
+        ; /// return form the cache(db)
+
+        /// not in DB - fetch from github api
+
+
+        try {
+            ResponseEntity<GitHubUser> response = restTemplate.getForEntity("https://api.github.com/users/" + login, GitHubUser.class);
+
+            GitHubUser user = response.getBody();
+
+            /// validate response
+
+            if (user != null && user.getId() != null ) {
+
+                githubUserRepo.save(user);
+                return user;
+            }else  {
+                throw new RuntimeException("User not found");
+            }
+
+        }catch (RuntimeException e) {
+            throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
+        }
+
+        /*
+        * . Complete flow
+First request for a user:
+
+DB check → not found
+
+Call GitHub API → success
+
+Save to DB
+
+Return user
+
+Subsequent requests for the same user:
+
+DB check → found
+
+Return user directly → no API call*/
+
+
     }
 }
